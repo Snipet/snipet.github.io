82394dec71d5ed70305cc2e9d9317721128d2402
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-18 14:29:38 -0400

    Update player.js

--- player.js
+++ player.js
@@ -1,3 +1,12 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+
 function load() {
   var id = getUrlVars()["id"];
   document.getElementById('player').scr = "https://scratch.mit.edu/projects/" + id + "embedded";
