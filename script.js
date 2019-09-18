146479b1420771dd9e88beea4dd1291bfd2dda51
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-18 17:55:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-
+function load(){
+    if (window.location == "https://snipet.github.io/index.html") {
+        document.getElementByTag('title').innerHTML = "Redirecting...";
+        window.location = "https://snipet.github.io";
+    }
+}
