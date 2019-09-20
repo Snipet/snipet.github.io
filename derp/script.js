133503d02d7cf9aed4edbeb39b17f7b70133e34c
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-20 14:12:09 -0400

    Update script.js

--- derp/script.js
+++ derp/script.js
@@ -2,4 +2,7 @@ function load(){
     var audio = new Audio('drop.wav');
     audio.play();
     document.getElementById('dropBoi').style.visibility = "hidden";
+    document.body.style.backgroundImage = "url('birb.gif')";
+    document.body.style.backgroundRepeat = "repeat";
+    
 }
