61df3a70224e6a12e23edea8761e037314946b24
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-21 16:22:28 -0400

    Update script.js

--- co/script.js
+++ co/script.js
@@ -3,7 +3,7 @@ function load(){
     if (window.location == "https://snipet.github.io/index.html") {
         window.location = "https://snipet.github.io";
     }else{
-        $("#area").load("/responsive/navhead.html");
+        $("#area").load("responsive/nav.html");
     }
     
 }
