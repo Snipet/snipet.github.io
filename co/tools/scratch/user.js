65cfec1a389381805a26c0e99529abd20e201563
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-21 19:36:26 -0400

    Update user.js

--- co/tools/scratch/user.js
+++ co/tools/scratch/user.js
@@ -1,1 +1,13 @@
+//variables
+var user = window.location.hash.substr(1);
+userJson = get("https://api.scratch.mit.edu/users/" + user);
+document.getElementById('pic');
+document.title = user;
+
+function get(url) {
+    fetch("https://cors-anywhere.herokuapp.com/" + url)
+    .then(response => response.text())
+    .then(contents => return contents)
+    .catch(() => console.log("Can’t access " + url + " response."))
+}
 
