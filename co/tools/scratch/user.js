28298c8c42d14a3cf64a6d73b6a8be0e9079b98d
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-21 22:29:49 -0400

    Update user.js

--- co/tools/scratch/user.js
+++ co/tools/scratch/user.js
@@ -11,4 +11,5 @@ function load() {
     userJson = JSON.parse(get("https://api.scratch.mit.edu/users/" + user));
     document.getElementById('pfp').src = "https://cdn2.scratch.mit.edu/get_image/user/" + userJson.id + "_120x120.png";
     document.title = user;
+    document.getElementById('link').src = "https://scratch.mit.edu/users/" + user;
 }
