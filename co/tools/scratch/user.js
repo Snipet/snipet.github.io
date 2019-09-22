53bcd10ef4958344697a04c80aa61605e4eeae0d
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-21 20:06:10 -0400

    Update user.js

--- co/tools/scratch/user.js
+++ co/tools/scratch/user.js
@@ -1,10 +1,3 @@
-//variables
-var user = window.location.hash.substr(1);
-userJson = JSON.parse(get("https://api.scratch.mit.edu/users/" + user));
-pic = document.getElementById('pic');
-document.title = user;
-pic.src = "https://cdn2.scratch.mit.edu/get_image/user/" + userJson.id + "_120x120.png";
-
 function get(url)
 {
     var xmlHttp = new XMLHttpRequest();
@@ -12,3 +5,12 @@ function get(url)
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
+
+function load() {
+    var user = window.location.hash.substr(1);
+    userJson = JSON.parse(get("https://api.scratch.mit.edu/users/" + user));
+    pic = document.getElementById('pic');
+    document.title = user;
+    pic.src = "https://cdn2.scratch.mit.edu/get_image/user/" + userJson.id + "_120x120.png";
+}
+window.onload = load();
