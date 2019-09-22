a954f80879cad590508d367081803ef101c51f00
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-22 14:14:57 -0400

    Update user.js

--- co/tools/scratch/user.js
+++ co/tools/scratch/user.js
@@ -6,11 +6,9 @@ function get(url)
     return xmlHttp.responseText;
 }
 
-function load() {
-    var user = window.location.hash.substr(1);
-    userJson = JSON.parse(get("https://api.scratch.mit.edu/users/" + user));
-    document.getElementById('pfp').src = "https://cdn2.scratch.mit.edu/get_image/user/" + userJson.id + "_120x120.png";
-    document.title = user;
-    document.getElementById('link').href = "https://scratch.mit.edu/users/" + user;
-    document.getElementById('username').innerHTML = user;
+function download() {
+    user = document.getElementById('username').value;
+    size = document.getElementById('size').value;
+    download = document.getElementById('downloader')
+    download.src = "userimage.html?user=" + user + "&size=" + size;
 }
