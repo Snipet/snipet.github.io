8575b66805edb35b77b33187aa16531e339d629b
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-23 14:25:44 -0400

    Update search.js

--- co/tools/scratch/search/search.js
+++ co/tools/scratch/search/search.js
@@ -25,17 +25,17 @@ function search() {
   var url = new URL(location.href);
   var term = url.searchParams.get("q");
   if (isNaN(term)){
-    console.log("term is not a number");
+    alert("ya yeee");
   }
 }
-function add(title, img, d){
+function add(title, img, desc){
   var a = document.createElement("h3")                // Create a <h1> element
   var b = document.createTextNode(title);     // Create a text node
   a.appendChild(b); 
   var c = document.createElement("img")                // Create a <h1> element
   c.src = img;
   var d = document.createElement("p")                // Create a <h1> element
-  var e = document.createTextNode(d);     // Create a text node
+  var e = document.createTextNode(desc);     // Create a text node
   d.appendChild(e)
   var f = document.createElement("div")
   f.appendChild(a)
