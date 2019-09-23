8c9336a5a08e68676064d3d040a409069144fb80
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-23 09:24:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -8,10 +8,14 @@ function load(){
     
 }
 
-function get(url)
-{
+function get(url){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", "https://cors-anywhere.herokuapp.com/" + url, false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
+}
+
+function loadAbout(){
+    var obj = JSON.parse(get("https://api.scratch.mit.edu/users/-Snipet-"));
+    document.getElementById('about').innerHTML = obj.bio;
 }
