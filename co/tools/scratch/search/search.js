562be29f5ff1109af0edf4493a5e218698329f2a
Sean <30637155+Snipet@users.noreply.github.com>
2019-09-23 12:44:11 -0400

    Update search.js

--- co/tools/scratch/search/search.js
+++ co/tools/scratch/search/search.js
@@ -1,3 +1,17 @@
-function search(){
+function goto(){
   location.href = "search.html?q=" + document.getElementById('term').value;
+}
+
+function get(url)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", "https://cors-anywhere.herokuapp.com/" + url, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
+}
+
+function search() {
+  var url = new URL(location.href);
+  var term = url.searchParams.get("q");
+  
 }
